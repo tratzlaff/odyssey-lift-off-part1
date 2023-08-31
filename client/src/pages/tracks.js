2ca67669e287f2be4dd9f8f3da260d42cd2a43e0
Tanner Ratzlaff <tratzlaff@gmail.com>
2023-08-31 13:41:13 -0500

    lift-off-part1 tutorial: The useQuery hook

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Layout } from '../components';
+import TrackCard from "../containers/track-card";
+
+// This lets us use query results in a consistent, predictable way throughout our app.
+import QueryResult from "../components/query-result";
 
 // We need to wrap all GraphQL strings in the gql template literal.
-import { gql } from "@apollo/client";
+// The useQuery hook is the primary API for executing queries in a React application,
+// so we'll use Apollo Client's useQuery hook.
+import { useQuery, gql } from "@apollo/client";
 
 const TRACKS = gql`
   query GetTracks {
@@ -26,7 +32,20 @@ const TRACKS = gql`
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
+    const { loading, error, data } = useQuery(TRACKS);
 
+    if (loading) return "Loading...";
+    if (error) return `Error! ${error.message}`;
+
+    return(
+        <Layout grid>
+            <QueryResult error={error} loading={loading} data={data}>
+                {data?.tracksForHome?.map((track) => (
+                    <TrackCard key={track.id} track={track} />
+                ))}
+            </QueryResult>
+        </Layout>
+    );
 };
 
 export default Tracks;
