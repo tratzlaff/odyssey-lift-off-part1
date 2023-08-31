0c821fca7689c6c2702ca14cca54d41d1f28a299
Tanner Ratzlaff <tratzlaff@gmail.com>
2023-08-31 10:53:30 -0500

    lift-off-part1 tutorial: cleanup tracks.js

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -4,10 +4,6 @@ import { Layout } from '../components';
 // We need to wrap all GraphQL strings in the gql template literal.
 import { gql } from "@apollo/client";
 
-/**
- * Tracks Page is the Catstronauts home page.
- * We display a grid of tracks fetched with useQuery with the TRACKS query
- */
 const TRACKS = gql`
   query GetTracks {
     tracksForHome {
@@ -24,5 +20,13 @@ const TRACKS = gql`
     }
   }
 `;
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+const Tracks = () => {
+
+};
 
 export default Tracks;
