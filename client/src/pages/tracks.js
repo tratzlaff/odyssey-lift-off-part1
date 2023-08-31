dbb9aeb8ed3c1646c06b2ea1b526312c53156b90
Tanner Ratzlaff <tratzlaff@gmail.com>
2023-08-31 10:48:09 -0500

    lift-off-part1 tutorial: Defining a query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Layout } from '../components';
 
+// We need to wrap all GraphQL strings in the gql template literal.
+import { gql } from "@apollo/client";
+
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
-const Tracks = () => {
-  return <Layout grid> </Layout>;
-};
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
 
 export default Tracks;
